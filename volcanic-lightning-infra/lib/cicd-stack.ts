967e18fd908c19cfad112741617a60f19f03ca6c
Hugo Page <hugo@Hugos-MacBook-Pro.local>
2024-11-10 17:59:09 +1300

    added frontend react app and code pipeline

---
+++ volcanic-lightning-infra/lib/cicd-stack.ts
@@ -0,0 +1,110 @@
+import { Construct } from 'constructs';
+import { NestedStack, SecretValue } from 'aws-cdk-lib';
+import { Pipeline, Artifact } from 'aws-cdk-lib/aws-codepipeline';
+import { GitHubSourceAction } from 'aws-cdk-lib/aws-codepipeline-actions';
+import { CodeBuildAction } from 'aws-cdk-lib/aws-codepipeline-actions';
+import { PipelineProject, BuildSpec, LinuxBuildImage, BuildEnvironmentVariableType } from 'aws-cdk-lib/aws-codebuild';
+import { CloudFrontWebDistribution } from 'aws-cdk-lib/aws-cloudfront';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
+
+interface Props {
+  spaHostingBucket: Bucket;
+  appCdn: CloudFrontWebDistribution;
+  appName: string;
+  githubSecretName: string;
+}
+
+interface IBuildProject {
+  id: string;
+  buildspec: string;
+  envVariables: {
+    [key: string]: {
+      type: BuildEnvironmentVariableType;
+      value: string;
+    };
+  };
+}
+
+export class CICDStack extends NestedStack {
+  private readonly stage: string;
+  private readonly appName: string;
+  constructor(scope: Construct, id: string, props: Props) {
+    super(scope, id);
+
+    const { spaHostingBucket, appCdn, appName, githubSecretName } = props;
+    this.appName = appName;
+    const sourceOutput = new Artifact(`pipeline-artifact`);
+
+    const sourceAction = new GitHubSourceAction({
+      actionName: 'GithubSource',
+      repo: 'volcanic-lightning-monitor',
+      owner: 'hugopage17',
+      oauthToken: SecretValue.secretsManager(githubSecretName),
+      branch: 'develop',
+      output: sourceOutput,
+    });
+
+    const sourceStage = {
+      stageName: 'Source',
+      actions: [sourceAction],
+    };
+
+    const buildArtifact = new Artifact(`build-artifact`);
+
+    const reactAppBuildProject = this.buildProject({
+      id: 'ReactAppBuildProject',
+      buildspec: 'frontend/buildspec.yml',
+      envVariables: {
+        S3_BUCKET: { type: BuildEnvironmentVariableType.PLAINTEXT, value: `s3://${spaHostingBucket.bucketName}` },
+        DISTRIBUTION_ID: { type: BuildEnvironmentVariableType.PLAINTEXT, value: appCdn.distributionId },
+        REPO: { type: BuildEnvironmentVariableType.PLAINTEXT, value: 'frontend' },
+      },
+    });
+
+    spaHostingBucket.grantReadWrite(reactAppBuildProject);
+    appCdn.grantCreateInvalidation(reactAppBuildProject);
+
+    const buildStage = {
+      stageName: 'Build',
+      actions: [
+        new CodeBuildAction({
+          actionName: 'ReactAppBuild',
+          project: reactAppBuildProject,
+          input: sourceOutput,
+          outputs: [buildArtifact],
+        }),
+      ],
+    };
+    
+
+    const pipeline = new Pipeline(this, `volcanic-lightning-pipeline`, {
+      pipelineName: `volcanic-lightning-pipeline`,
+      stages: [sourceStage, buildStage],
+    });
+  }
+
+  private buildProject({ id, buildspec, envVariables }: IBuildProject) {
+    return new PipelineProject(this, `${this.appName}-${id}-pipeline-project`, {
+      buildSpec: BuildSpec.fromSourceFilename(buildspec),
+      environment: {
+        buildImage: LinuxBuildImage.STANDARD_7_0,
+        privileged: true,
+        environmentVariables: { ...envVariables },
+      },
+    //   role: new Role(this, `${this.appName}-${id}-build-project-role-${this.stage}`, {
+    //     roleName: `${this.appName}-${id}-build-project-role-${this.stage}`,
+    //     assumedBy: new ServicePrincipal('codebuild.amazonaws.com'),
+    //     inlinePolicies: {
+    //       RolePolicy: new PolicyDocument({
+    //         statements: [
+    //           new PolicyStatement({
+    //             resources: ['*'],
+    //             actions: ['s3:*', 'cloudformation:*', 'iam:*', 'lambda:*'],
+    //           })
+    //         ],
+    //       }),
+    //     },
+    //   }),
+    });
+  }
+}
--- volcanic-lightning-infra/lib/volcanic-lightning-infra-stack.ts
+++ volcanic-lightning-infra/lib/volcanic-lightning-infra-stack.ts
@@ -1,16 +1,83 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { NestedStack, RemovalPolicy } from 'aws-cdk-lib';
+import { Bucket, BlockPublicAccess, BucketAccessControl } from 'aws-cdk-lib/aws-s3';
+import { CloudFrontWebDistribution, OriginAccessIdentity, CloudFrontAllowedMethods, CloudFrontAllowedCachedMethods } from 'aws-cdk-lib/aws-cloudfront';
+import { CloudFrontTarget } from 'aws-cdk-lib/aws-route53-targets';
+import { HostedZone, ARecord, RecordTarget } from 'aws-cdk-lib/aws-route53';
+import { Secret } from 'aws-cdk-lib/aws-secretsmanager';
 import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { CICDStack } from './cicd-stack';
 
 export class VolcanicLightningInfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const historyTable = new Table(this, 'volcanic-lightning-table', {
-      tableName: 'volcanic-lightning-history',
+    const APP_NAME = 'volcanic-lightning'
+
+    const historyTable = new Table(this, `${APP_NAME}-table`, {
+      tableName: `${APP_NAME}-history`,
       partitionKey: { name: 'pk', type: AttributeType.STRING },
       sortKey: { name: 'sk', type: AttributeType.STRING },
       timeToLiveAttribute: 'TTL'
     });
+
+    const spaHostingBucket = new Bucket(this, `${APP_NAME}-hosting-bucket`, {
+      bucketName: `${APP_NAME}-hosting-bucket`,
+      websiteIndexDocument: 'index.html',
+      blockPublicAccess: BlockPublicAccess.BLOCK_ACLS,
+      accessControl: BucketAccessControl.BUCKET_OWNER_FULL_CONTROL,
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+
+    const originAccessIdentity = new OriginAccessIdentity(this, `${id}-OriginAccessIdentity`);
+    const appCdn = new CloudFrontWebDistribution(this, `${id}-WebsiteDistribution`, {
+      originConfigs: [
+        {
+          s3OriginSource: {
+            s3BucketSource: spaHostingBucket,
+            originAccessIdentity,
+          },
+          behaviors: [
+            {
+              isDefaultBehavior: true,
+              allowedMethods: CloudFrontAllowedMethods.GET_HEAD_OPTIONS,
+              cachedMethods: CloudFrontAllowedCachedMethods.GET_HEAD_OPTIONS,
+              forwardedValues: {
+                queryString: false,
+                cookies: {
+                  forward: 'none',
+                },
+              },
+            },
+          ],
+        },
+      ],
+      errorConfigurations: [
+        {
+          errorCode: 403,
+          responseCode: 200,
+          responsePagePath: '/index.html',
+          errorCachingMinTtl: 60,
+        },
+        {
+          errorCode: 404,
+          responseCode: 200,
+          responsePagePath: '/index.html',
+          errorCachingMinTtl: 60,
+        },
+      ],
+    });
+
+    const githubSecret = new Secret(this, 'github-auth-secret', {
+        secretName: 'github/oauth/secret'
+    })
+
+    new CICDStack(this, 'volcanic-lightning-cicd-stack', {
+        spaHostingBucket,
+        appName: APP_NAME,
+        appCdn,
+        githubSecretName: githubSecret.secretName
+    })
   }
 }
